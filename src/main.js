50efacd121c3d1414f6c040b116eec42a063cc94
amaury2409 <amaury.henrottay@gmail.com>
2019-11-22 00:48:57 +0100

    store my todolist

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import {
+  store
+} from './store/store'
 
 window.eventBus = new Vue()
 
@@ -11,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store,
   components: {
     App
   },
