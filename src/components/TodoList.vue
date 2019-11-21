0400ad92a97393ffcd6730d287f5d81ddeeea952
amaury2409 <amaury.henrottay@gmail.com>
2019-11-21 14:34:00 +0100

    yarn install && sass style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,18 +1,106 @@
 <template>
-  <div>todo list goes here</div>
+  <div>
+    <input type="text" class="todo-input" placeholder="what needs to be done" v-model="newTodo"
+      @keyup.enter="addTodo" />
+
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      <div>
+        {{ todo.title }}
+      </div>
+      <div class="remove-item">
+        &times;
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: "todo-list",
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
-    };
-  }
-};
+  export default {
+    name: "todo-list",
+    data() {
+      return {
+        newTodo: "",
+        idForTodo: 3,
+        todos: [{
+            id: 1,
+            title: "Finish Vue Screencast",
+            completed: false
+          },
+          {
+            id: 2,
+            title: "Take over world",
+            completed: false
+          }
+        ]
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim().length == 0) {
+          return
+        }
+
+        this.todos.push({
+          id: this.idForTodo,
+          title: this.newTodo,
+          completed: false
+        })
+        this.newTodo = '',
+          this.idForTodo++
+      }
+    }
+  };
+
 </script>
 
+
+
+
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style lang="scss">
+  html {
+    width: 100%;
+    height: 100%;
+    background: rgb(77, 184, 128);
+    background: linear-gradient(157deg, rgba(77, 184, 128, 1) 0%, rgba(69, 145, 116, 1) 52%, rgba(53, 73, 94, 1) 100%);
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+
+  .todo-input {
+
+
+    width: 100%;
+    padding: 10px 20px;
+    font-size: 20px;
+    margin-bottom: 20px;
+    border-radius: 25px 25px 25px 25px;
+    border: none;
+    box-shadow: 0 0 0 2px #35495e,
+      0 0 0 4px #4DB883,
+      2px 2px 19px rgba(0, 0, 0, 0.24);
+
+    &:focus {
+      outline: 0;
+    }
+  }
+
+  .todo-item {
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+  }
+
+  .remove-item:hover {
+    color: black;
+    transform: scale(2);
+    transition-duration: 0.5s;
+  }
+
 </style>
